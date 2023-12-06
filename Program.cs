039421843eee537fc0b2e65ea3df9a9af80cf2c9
Felipe B <felipebastosdesouza@gmail.com>
2023-12-06 11:02:12 -0300

    Finalizado com calculos

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace Stopwatch
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -9,19 +9,34 @@
 
         static void Menu()
         {
+            Console.Clear();
             Console.WriteLine("S = Segundos => 10s = 10 segundos");
             Console.WriteLine("M = Minutos => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower();
-            char type = char.Parse(data.Substring(data.Length-1, 1));
-            Console.WriteLine(data);
-            Console.WriteLine(type);
+            char type = char.Parse(data.Substring(data.Length -1, 1)); //pega o primeiro dígito contando a posição 012345 e subtrai a letra "s"
+            int time = int.Parse(data.Substring(0, data.Length - 1));  //pega somente ó número digitado e subtrai a letra "s" para prosseguir com cálculo
+            int multiplicador = 1;
+
+            if (type == 'm')
+            {
+                multiplicador = 60;
+            }
+
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            Inicio(time * multiplicador);
+
         }
 
         static void Inicio(int tempo) 
         {
+            
             int minutoatual = 0;
 
             while (minutoatual != tempo)
@@ -32,9 +47,11 @@
                 Thread.Sleep(1000);
             }
 
-            Console.Clear() ;
+            
             Console.WriteLine("Stopwatch finalizado.");
-            Thread.Sleep(2500);
+            Thread.Sleep(5000);
+
+            Menu();
         }        
     }
 }
